5f156c68c33950488b00ff8747e346e610984a1c
Abdulhadi Alzehhawi <W0877365@myscc.ca>
2026-02-06 18:50:02 -0500

    updated content list fetch data from CarService and added CRUD methods

--- src/app/car.list/car.list.component.ts
+++ src/app/car.list/car.list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Cars } from '../cars';
 import { CarListItemComponent } from '../car.list.item/car.list.item.component';
@@ -11,16 +11,16 @@ import { CarService } from '../services/car.service';
   templateUrl: './car.list.component.html',
   styleUrls: ['./car.list.component.css']
 })
-export class CarListComponent {
-  carsList: Cars[] = [
-    { id: 1, name: "Giulia", company: "Alfa Romeo", year: 2026, electric: false },
-    { id: 2, name: "Mustang", company: "Ford", year: 1969, electric: false },
-    { id: 3, name: "Santa Fe", company: "Hyundai", year: 2008, electric: false },
-    { id: 4, name: "Camry", company: "Toyota", year: 2007, electric: false },
-    { id: 5, name: "Model 3", company: "Tesla", year: 2025, electric: true },
-    { id: 6, name: "Accord", company: "Honda", year: 2020, electric: false },
-    { id: 7, name: "GT-R", company: "Nissan", year: 2024, electric: false }
-  ];
+export class CarListComponent implements OnInit {
+  carsList: Cars[] = [];
 
   constructor(private carService: CarService) { }
+
+  ngOnInit(): void {
+    this.carService.getCars().subscribe({
+      next: (data: Cars[]) => this.carsList = data,
+      error: err => console.error("Error fetching cars", err),
+      complete: () => console.log("Car data fetch complete")
+    });
+  }
 }
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -7,10 +7,34 @@ import { CARS } from '../data/mock-content';
   providedIn: 'root'
 })
 export class CarService {
+  private carsList: Cars[] = CARS;
 
   constructor() { }
 
   getCars(): Observable<Cars[]> {
     return of(CARS);
+  }
+
+  getCar(id: number): Observable<Cars | undefined> {
+    const car = this.carsList.find(c => c.id === id);
+    return of(car);
+  }
+
+  createCar(car: Cars): Observable<Cars[]> {
+    this.carsList.push(car);
+    return of(this.carsList);
+  }
+
+  updateCar(car: Cars): Observable<Cars[]> {
+    const index = this.carsList.findIndex(c => c.id === car.id);
+    if (index !== -1) {
+      this.carsList[index] = car;
+    }
+    return of(this.carsList);
+  }
+
+  deleteCar(id: number): Observable<Cars[]> {
+    this.carsList = this.carsList.filter(c => c.id !== id);
+    return of(this.carsList);
   }
 }
