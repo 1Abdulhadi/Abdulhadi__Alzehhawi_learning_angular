b03d4271eb501c0cbf1e8966f5b601a34f5a7c1e
Abdulhadi Alzehhawi <W0877365@myscc.ca>
2026-02-06 17:51:24 -0500

    Created mock content file and car service files

---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,11 @@
+import { Cars } from '../cars';
+
+export const CARS: Cars[] = [
+  { id: 1, name: "Giulia", company: "Alfa Romeo", year: 2026, electric: false },
+  { id: 2, name: "Mustang", company: "Ford", year: 1969, electric: false },
+  { id: 3, name: "Santa Fe", company: "Hyundai", year: 2008, electric: false },
+  { id: 4, name: "Camry", company: "Toyota", year: 2007, electric: false },
+  { id: 5, name: "Model 3", company: "Tesla", year: 2025, electric: true },
+  { id: 6, name: "Accord", company: "Honda", year: 2020, electric: false },
+  { id: 7, name: "GT-R", company: "Nissan", year: 2024, electric: false }
+];
