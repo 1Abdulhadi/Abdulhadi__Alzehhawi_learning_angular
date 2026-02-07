c895cd9586f385d20f7cea1e9d4fa71fadf676e3
Abdulhadi Alzehhawi <W0877365@myscc.ca>
2026-02-06 19:21:31 -0500

    added single content list item to app component and populated it

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarListComponent } from './car.list/car.list.component';
+import { CarListItemComponent } from './car.list.item/car.list.item.component';
+import { CarService } from './services/car.service';
+import { Cars } from './cars';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CarListComponent],
+  imports: [CarListComponent, CarListItemComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   title = 'Car Management System';
+  selectedCar?: Cars;
+
+  constructor(private carService: CarService) { }
+
+  ngOnInit(): void {
+    this.carService.getCar(1).subscribe({
+      next: (data: Cars | undefined) => this.selectedCar = data,
+      error: err => console.error("Error fetching car", err),
+      complete: () => console.log("Single car fetch complete!")
+    });
+  }
 }
