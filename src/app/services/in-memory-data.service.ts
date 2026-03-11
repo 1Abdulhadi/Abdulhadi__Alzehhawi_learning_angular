77caef89a04f78b45705b9222de000164aedf132
Abdulhadi Alzehhawi <W0877365@myscc.ca>
2026-03-11 03:25:34 -0400

    installed angular-in-memory-web-api and created InMemoryDataService with car data

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Cars } from "../cars";
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb(): { cars: Cars[] } {
+    const cars: Cars[] = [
+      { id: 1, name: "Giulia", company: "Alfa Romeo", year: 2026, electric: false, imageUrl: "https://di-uploads-pod27.dealerinspire.com/genesisalfaromeo/uploads/2023/11/gar1114a-300x189.png" },
+      { id: 2, name: "Mustang", company: "Ford", year: 1969, electric: false, imageUrl: "https://silodrome.com/wp-content/uploads/2025/03/1969-Ford-Mustang-Fastback.jpg" },
+      { id: 3, name: "Santa Fe", company: "Hyundai", year: 2008, electric: false, imageUrl: "https://www.edmunds.com/assets/m/hyundai/santa-fe/2008/oem/2008_hyundai_santa-fe_4dr-suv_limited_fq_oem_1_500.jpg" },
+      { id: 4, name: "Camry", company: "Toyota", year: 2007, electric: false, imageUrl: "https://file.kelleybluebookimages.com/kbb/base/house/2007/2007-Toyota-Camry-FrontSide_TOCAM071_505x375.jpg" },
+      { id: 5, name: "Model 3", company: "Tesla", year: 2025, electric: true, imageUrl: "https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Model-3-Main-Hero-Desktop-LHD.jpg" },
+      { id: 6, name: "Accord", company: "Honda", year: 2020, electric: false, imageUrl: "https://cdn.jdpower.com/JDP_2024%20Honda%20Accord%20EX%20Blue%20Front%20Quarter%20View.jpg" },
+      { id: 7, name: "GT-R", company: "Nissan", year: 2024, electric: false, imageUrl: "https://static.tcimg.net/vehicles/oem/3b430e971702ba2b/2024-Nissan-GT-R.jpg?auto=format&fit=crop&h=514&w=720" }
+    ];
+    return { cars };
+  }
+}
